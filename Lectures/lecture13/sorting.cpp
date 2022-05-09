f857a05574bb80821a26ea11a227beab71df2d42
lazarulian <69273628+lazarulian@users.noreply.github.com>
2022-05-09 15:51:12 -0700

    Lecture 13 - Quicksort
    Learned: Insertion Sort, Bubble Sort, Selection Sort, Quick Sort

---
+++ Lectures/lecture13/sorting.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <vector>
+
+
+using namespace std;
+
+// Selection Sort o(n^2)
+
+// passing through finding the largest in the vector, then putting it at the 
+// end of the vector
+
+// repeating the process when getting rid of the last item in the vector since we know
+// it is the largest
+
+list<int> SelectionSort(list <int> input)
+{ int counter = input.size();
+    while (counter != 0)
+    {
+        for (int i = 0; i < )
+    }
+}
+
+
+
+// Bubble Sort 
+// worst case: o(n^2) best case: o(n)
+// if they are in the decending order, it will take 
+
+// check the first two elements, and keep swapping the smallest and the largest
+    // the biggest item will always be in the right place at the end of the algorithm
+
+
+
+//// Insertion Sort o(n^2)
+// Imagine you are holding some cards, and you draw a card,
+// you will add it into the hand in the right order
+// if the array is almost sorted, like all of the items are within
+// three items away from their spot, it is o(n)
+
+
+list<int> insertionSort(list <int> input)
+{
+    list <int> returnList;
+    returnList.push_back();
+
+}
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+    return 0;
+}
